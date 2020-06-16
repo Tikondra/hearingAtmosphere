d30baddf1ec9e815c9f4c12a7e50880d0d074782
Tikondra <tikondra47@gmail.com>
2020-06-16 18:14:30 +0400

    save

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,30 @@
     btnSvg.classList.toggle('active');
   });
 })();
+
+if (document.querySelector('.filters')) {
+  // фильтр
+  var filters = document.querySelector('.filters');
+  var filterOpen = document.querySelector('.catalog__filter-btn');
+  var filterClose = filters.querySelector('.filters__btn-close');
+
+  filterOpen.addEventListener('click', function () {
+    filters.classList.add('filters--open');
+  });
+
+  filterClose.addEventListener('click', function () {
+    filters.classList.remove('filters--open');
+  });
+};
+
+if (document.querySelector('.card-description')) {
+  var description = document.querySelector('.card-description');
+  var openBtn = description.querySelector('.card-description__open');
+  var descriptionList = description.querySelector('.card-description__list');
+
+  openBtn.addEventListener('click', function () {
+    descriptionList.classList.toggle('card-description__list--show');
+    openBtn.classList.toggle('card-description__open--active');
+    description.classList.toggle('card-description--show');
+  })
+}
