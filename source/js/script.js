9b83e73935eba6a53fce033293d6bec5dd166866
Tikondra <tikondra47@gmail.com>
2020-06-22 13:30:44 +0400

    contacts js

--- source/js/script.js
+++ source/js/script.js
@@ -42,9 +42,9 @@ if (document.querySelector('.card-description')) {
 }
 
 // форма заказа
-if (document.querySelector('.card')) {
+if (document.querySelector('#order')) {
   var modal = document.querySelector('.overlay');
-  var open = document.querySelector('.card__btn');
+  var open = document.querySelector('.js-open');
   var close = modal.querySelector('.modal-order__close');
   var body = document.querySelector('body');
 
@@ -60,7 +60,6 @@ if (document.querySelector('.card')) {
   })
 
   modal.addEventListener('click', function(evt) {
-    console.log(evt.target)
     if (evt.target === modal) {
       modal.classList.remove('overlay--show');
       body.classList.remove('scroll-stop');
@@ -75,3 +74,85 @@ if (document.querySelector('.card')) {
     }
   });
 }
+
+// local
+(function () {
+  var citySelect = document.querySelector('.header__city select');
+  var headerAddress = document.querySelector('.header__address');
+  var footerAddress = document.querySelector('.footer__address span');
+  var contactsCitySelect = document.querySelector('.contacts__city select');
+
+  function changeForm (val) {
+    var form = document.querySelector('.map__info');
+    var address = form.querySelector('.map__address');
+    var phone1 = form.querySelector('.map__phone--1 span');
+    var phone2 = form.querySelector('.map__phone--2 span');
+    var phoneLink1 = form.querySelector('.map__phone--1');
+    var phoneLink2 = form.querySelector('.map__phone--2');
+
+    if (val === 'Astrachan') {
+      address.textContent = 'ул. Боевая, 14';
+      phone1.textContent = '+7(988) 172-87-76';
+      phone2.textContent = '+7(8512) 42-87-76';
+      phoneLink1.href = 'tel:89881728776';
+      phoneLink2.href = 'tel:88512428776';
+    }
+    if (val === 'Saratov') {
+      address.textContent = 'ул. Чернышевского, 203';
+      phone1.textContent = '+7(908) 551–63–60';
+      phone2.textContent = '+7(8452) 71–25–10';
+      phoneLink1.href = 'tel:89085516360';
+      phoneLink2.href = 'tel:88452712510';
+    }
+    if (val === 'Stavrapol') {
+      address.textContent = 'ул. Мира, 276';
+      phone1.textContent = '+7(962) 455–82–99';
+      phone2.textContent = '+7(8652) 65–82–99';
+      phoneLink1.href = 'tel:89624558299';
+      phoneLink2.href = 'tel:88652658299';
+    }
+    if (val === 'Tymen') {
+      address.textContent = 'ул. Республики, 61';
+      phone1.textContent = '+7(952) 678–93–94';
+      phone2.textContent = '+7(3452) 96–93–94';
+      phoneLink1.href = 'tel:89526789394';
+      phoneLink2.href = 'tel:83452969394';
+    }
+  }
+
+  function changeInfo (val) {
+    if (val === 'Astrachan') {
+      headerAddress.textContent = 'ул. Боевая, 14';
+      footerAddress.textContent = 'ул. Боевая, 14';
+    }
+    if (val === 'Saratov') {
+      headerAddress.textContent = 'ул. Чернышевского, 203';
+      footerAddress.textContent = 'ул. Чернышевского, 203';
+    }
+    if (val === 'Stavrapol') {
+      headerAddress.textContent = 'ул. Мира, 276';
+      footerAddress.textContent = 'ул. Мира, 276';
+    }
+    if (val === 'Tymen') {
+      headerAddress.textContent = 'ул. Республики, 61';
+      footerAddress.textContent = 'ул. Республики, 61';
+    }
+  }
+
+  citySelect.addEventListener('change', function(evt) {
+    changeInfo(evt.target.value);
+
+    if (contactsCitySelect) {
+      contactsCitySelect.value = evt.target.value;
+      changeForm(evt.target.value);
+    }
+  })
+
+  if (contactsCitySelect) {
+    contactsCitySelect.addEventListener('change', function(evt) {
+      citySelect.value = evt.target.value;
+      changeInfo(evt.target.value);
+      changeForm(evt.target.value);
+    })
+  }
+})();
